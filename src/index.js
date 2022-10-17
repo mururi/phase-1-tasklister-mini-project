dd30380d7c915497bea36b8be1d4d14269b2e7bc
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-10-18 01:41:20 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form = document.getElementById('create-task-form');
+  ul = document.getElementById('tasks');
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let newTask = form.querySelector('#new-task-description').value;
+    li = document.createElement('li');
+    li.textContent = `${newTask}  `;
+    button = document.createElement('button');
+    button.textContent = 'X';
+    button.addEventListener("click", (event) => {
+      event.target.parentNode.remove();
+    });
+    li.appendChild(button);
+    ul.appendChild(li);
+    form.reset();
+  })
 });
